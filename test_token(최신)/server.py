221456a088d21d9a02182d7e845a23686f57d405
LAPTOP-V37U6R4R\seongeun <seongeun0@naver.com>
2018-08-29 13:13:52 +0900

    server.py 파일 생성
    python으로 smartcontract 배포 후 / flask(server.py)로 웹 제작
    
    index.html : main page
    signup.html : signup page
    login.html : login page
    upload.html : upload page
    user.html : login 후 session을 통한 각 user page
    admin.html  : admin 계정으로 login 시 볼수 있는 admin page

---
+++ test_token(최신)/server.py
@@ -0,0 +1,170 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory
+from flask import render_template,flash,session
+from werkzeug.utils import secure_filename
+from dapp_token import *
+import pymysql
+
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['mp3'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'seongeun_secret'
+
+# token name = eun
+eun = mytoken('MyBasicToken.sol','MyBasicToken')
+
+## file upload
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+## main page
+@app.route('/', methods=['GET','POST'])
+def index():
+    path_dir ='./uploads'
+    music_list = os.listdir(path_dir)
+    show_token = eun.show_token(w3.eth.accounts[0])
+    # login o
+    if session:
+        user_name = session['user']
+        conn = pymysql.connect(host='localhost', user='root',passwd='qwer1234',db='mydapp',charset='utf8')
+        cursor = conn.cursor()
+        query = 'SELECT wallet FROM user_table WHERE name = %s'
+        value = (user_name)
+        cursor.execute(query,value)
+        user_wallet = (cursor.fetchall())
+        cursor.close()
+        conn.close()
+        user_wallet = user_wallet[0][0]
+        show_token = eun.show_token(user_wallet)
+
+        # admin login
+        if user_name == 'admin':
+            user_list = w3.eth.accounts
+            now_block_number = w3.eth.blockNumber
+
+            if request.method == 'POST' and 'user_name_balance' in request.form:
+                addr_balance =  request.form['user_name_balance']
+                show_token = eun.show_token(addr_balance)
+            if request.method == 'POST' and 'user_name_transfer' in request.form and 'amount' in request.form:
+                addr_transfer = request.form['user_name_transfer']
+                amount_transfer = int(request.form['amount'])
+                eun.send_token(w3.eth.accounts[0],addr_transfer,amount_transfer)
+
+            totalSupply = eun.show_total_token()
+            return render_template('admin.html',totalSupply=totalSupply,user_list=user_list,show_token=show_token,now_block_number =now_block_number)#
+
+        return render_template('user.html',music_list=music_list, user_name = user_name,user_wallet=user_wallet,show_token=show_token)
+
+
+    # login x
+    else:
+        return render_template('index.html',music_list= music_list)
+
+
+# logout page
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect(url_for('index'))
+
+
+# login page
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        pw = request.form['pw']
+        conn = pymysql.connect(host='localhost', user='root',passwd='qwer1234',db='mydapp',charset='utf8')
+        cursor = conn.cursor()
+        query = 'SELECT name FROM user_table WHERE email = %s AND pw = %s'
+        value = (email,pw)
+        cursor.execute('set names utf8')
+        cursor.execute(query,value)
+        data = (cursor.fetchall())
+        cursor.close()
+        conn.close()
+        for row in data:
+            data = row[0]
+            if data:
+                session['user'] = data
+                return redirect(url_for('index'))
+        else:
+            return 0
+            #flash('Invalid input data detected!')
+    return render_template('login.html')
+
+# sign-up page
+@app.route('/signup', methods=['GET','POST'])
+def sign_up():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        pw = request.form['pw']
+
+        conn = pymysql.connect(host='localhost',user='root', passwd='qwer1234', db='mydapp',charset='utf8')
+        cursor = conn.cursor()
+
+        query = "SELECT 1 FROM user_table WHERE email='%s'"%(email)
+        #value = (email)
+        cursor.execute(query)
+        data = cursor.fetchall()
+        if data:
+            #flash('user other email')
+            error = 'The email is already userd. please use another one'
+        else:
+            # sign-up 시 지갑 주소 생성 제공!
+            wallet_key = w3.personal.newAccount(pw)
+            query = 'INSERT INTO user_table (name,email,pw,wallet) value(%s,%s,%s,%s)'
+            value = (name,email,pw,wallet_key)
+            cursor.execute(query,value)
+            data = cursor.fetchall()
+
+
+
+            if not data:
+                conn.commit()
+            else:
+                conn.rollback()
+                return print('Register Failed')
+            return redirect(url_for('login'))
+
+
+        cursor.close()
+        conn.close()
+    return render_template('signup.html')
+
+
+
+
+# upload page
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('no file')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print('no filename')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return render_template('upload.html')
+
+
+'''
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+'''
+
+
+if __name__ == '__main__':
+    app.run('localhost',5000, debug = True)
